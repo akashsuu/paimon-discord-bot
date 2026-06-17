2c50260285679a2f220624748df16ac642ffa277
akashsuu <demonzone2021@gmail.com>
2026-06-17 22:20:08 +0530

    update commands

--- commands/information/sex.js
+++ commands/information/sex.js
@@ -1,42 +1,61 @@
 const axios = require('axios');
 
 module.exports = {
-    name: 'gay',
-    aliases: [],
+    name: 'sex',
+    aliases: ['fuck'],
     category: 'info',
     cooldown: 5,
     premium: false,
     run: async (client, message, args) => {
-        const msg = await message.channel.send('Fetching gay anime images...');
+        const msg = await message.channel.send('Fetching...');
 
         try {
+            let image;
+
             const { data } = await axios.get('https://api.nekosapi.com/v4/images/random', {
                 params: {
-                    tags: 'boy,dick',
-                    without_tags: 'girl,exposed_girl_breasts,pussy,futanari,yuri',
+                    tags: 'dick,pussy',
                     rating: 'explicit',
-                    limit: 1,
+                    limit: 5,
                 },
             });
 
-            let image = data?.[0];
+            if (data?.length > 0) {
+                image = data[Math.floor(Math.random() * data.length)];
+            }
+
             if (!image) {
                 const { data: fallback } = await axios.get('https://api.nekosapi.com/v4/images/random', {
                     params: {
-                        tags: 'boy,kissing',
+                        tags: 'dick,anal',
                         rating: 'explicit',
-                        limit: 1,
+                        limit: 5,
                     },
                 });
-                image = fallback?.[0];
+                if (fallback?.length > 0) {
+                    image = fallback[Math.floor(Math.random() * fallback.length)];
+                }
             }
+
+            if (!image) {
+                const { data: fallback2 } = await axios.get('https://api.nekosapi.com/v4/images/random', {
+                    params: {
+                        tags: 'threesome',
+                        rating: 'explicit',
+                        limit: 5,
+                    },
+                });
+                if (fallback2?.length > 0) {
+                    image = fallback2[Math.floor(Math.random() * fallback2.length)];
+                }
+            }
+
             if (!image) {
                 return msg.edit('No images found.');
             }
 
             const embed = client.util.embed()
                 .setColor(client.color)
-                .setTitle('Gay Anime')
                 .setURL(image.url)
                 .setImage(image.url)
                 .setFooter({ text: `Image ${image.id}  ·  ${image.rating}` });
